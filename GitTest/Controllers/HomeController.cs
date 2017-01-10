5a711f0934fec8d611802dc3d505822ba7d9a058
Cristian Marín <cm961224@gmail.com>
2017-01-10 07:43:03 -0500

    commit

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace GitTest.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            //Esto es un cambio en develop
             return View();
         }
 
